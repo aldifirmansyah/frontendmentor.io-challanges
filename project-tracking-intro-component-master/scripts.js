7c22383e0be8391cf731d0de9c39706c4339f8a8
aldifirmansyah <aldifirmansyh@gmail.com>
2019-10-18 11:39:49 +0700

    progress

--- project-tracking-intro-component-master/scripts.js
+++ project-tracking-intro-component-master/scripts.js
@@ -1,13 +1,18 @@
-const burger = document.getElementById("burger");
+const burgerChildren = document.getElementById("burger").children;
+const burgerClose = burgerChildren[0];
+console.log(burgerClose);
+const burgerOpen = burgerChildren[1];
 const nav = document.getElementsByTagName("nav")[0];
 
 function burgerClicked() {
-	if (burger.classList.contains("open")) {
-		burger.classList.remove("open");
+	if (burgerClose.classList.contains("hidden")) {
+		burgerClose.classList.remove("hidden");
+		burgerOpen.classList.add("hidden");
 		nav.classList.add("close");
 	}
 	else {
-		burger.classList.add("open");
+		burgerClose.classList.add("hidden");
+		burgerOpen.classList.remove("hidden");
 		nav.classList.remove("close");
 	}
 }
